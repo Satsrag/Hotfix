b61b9332843a21778b3f2a74d61884b7132cbff9
Saqrag <saqrag@outlook.com>
2017-05-10 13:03:51 +0800

    fix patch

--- app/src/main/java/com/zuga/hotfix/MainActivity.java
+++ app/src/main/java/com/zuga/hotfix/MainActivity.java
@@ -16,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadPatch(View view) {
-        TinkerInstaller.onReceiveUpgradePatch(getApplication(), Environment.getExternalStorageDirectory() + "patch.apk");
+        TinkerInstaller.onReceiveUpgradePatch(getApplication(), Environment.getExternalStorageDirectory() + "/patch.apk");
     }
 }
