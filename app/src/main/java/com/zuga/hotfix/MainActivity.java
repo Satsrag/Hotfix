647ffc91cece56391b7d8438b9f19b1735b0b5a4
Saqrag <saqrag@outlook.com>
2017-05-12 17:22:45 +0800

    end

--- app/src/main/java/com/zuga/hotfix/MainActivity.java
+++ app/src/main/java/com/zuga/hotfix/MainActivity.java
@@ -2,7 +2,6 @@ package com.zuga.hotfix;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,7 +18,6 @@ import com.tencent.tinker.lib.tinker.Tinker;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
-import com.zuga.hotfix.hotfix.BaseBuildInfo;
 import com.zuga.hotfix.hotfix.BuildInfo;
 import com.zuga.hotfix.hotfix.util.Utils;
 
@@ -61,35 +59,8 @@ public class MainActivity extends PermissionActivity {
     }
 
     public void showInfo(View view) {
-        showInfo(this);
-    }
-
-    private boolean showInfo(Context context) {
-        // add more Build Info
-        final StringBuilder sb = new StringBuilder();
-        Tinker tinker = Tinker.with(getApplicationContext());
-        if (tinker.isTinkerLoaded()) {
-            sb.append(String.format("[patch is loaded] \n"));
-            sb.append(String.format("[buildConfig TINKER_ID] %s \n", BuildInfo.TINKER_ID));
-            sb.append(String.format("[buildConfig BASE_TINKER_ID] %s \n", BaseBuildInfo.BASE_TINKER_ID));
-
-            sb.append(String.format("[buildConfig MESSSAGE] %s \n", BuildInfo.MESSAGE));
-            sb.append(String.format("[TINKER_ID] %s \n", tinker.getTinkerLoadResultIfPresent().getPackageConfigByName(ShareConstants.TINKER_ID)));
-            sb.append(String.format("[packageConfig patchMessage] %s \n", tinker.getTinkerLoadResultIfPresent().getPackageConfigByName("patchMessage")));
-            sb.append(String.format("[TINKER_ID Rom Space] %d k \n", tinker.getTinkerRomSpace()));
-
-        } else {
-            sb.append(String.format("[patch is not loaded] \n"));
-            sb.append(String.format("[buildConfig TINKER_ID] %s \n", BuildInfo.TINKER_ID));
-            sb.append(String.format("[buildConfig BASE_TINKER_ID] %s \n", BaseBuildInfo.BASE_TINKER_ID));
-
-            sb.append(String.format("[buildConfig MESSSAGE] %s \n", BuildInfo.MESSAGE));
-            sb.append(String.format("[TINKER_ID] %s \n", ShareTinkerInternals.getManifestTinkerID(getApplicationContext())));
-        }
-        sb.append(String.format("[BaseBuildInfo Message] %s \n", BaseBuildInfo.TEST_MESSAGE));
-
-        final TextView v = new TextView(context);
-        v.setText(sb);
+        final TextView v = new TextView(this);
+        v.setText(String.format("baseId: %s \npatchId %s\nisLoaded: %s", getBaseId(), getPatchId(), isLoaded()));
         v.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
         v.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10);
         v.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -98,11 +69,27 @@ public class MainActivity extends PermissionActivity {
         final int padding = 16;
         v.setPadding(padding, padding, padding, padding);
 
-        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(true);
         builder.setView(v);
         final AlertDialog alert = builder.create();
         alert.show();
-        return true;
+    }
+
+    private String getBaseId() {
+        final Tinker tinker = Tinker.with(getApplicationContext());
+        if (tinker.isTinkerLoaded()) {
+            return tinker.getTinkerLoadResultIfPresent().getPackageConfigByName(ShareConstants.TINKER_ID);
+        } else {
+            return ShareTinkerInternals.getManifestTinkerID(getApplicationContext());
+        }
+    }
+
+    private String getPatchId() {
+        return BuildInfo.TINKER_ID;
+    }
+
+    private boolean isLoaded() {
+        return Tinker.with(getApplicationContext()).isTinkerLoaded();
     }
 }
